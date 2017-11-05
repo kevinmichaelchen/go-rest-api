7c1c9b1743049a38ace992c14676202f3cea62b9
Kevin Chen <kevinmichaelchen@gmail.com>
2017-11-05 18:57:20 -0500

    hello world route

--- app.go
+++ app.go
@@ -37,11 +37,16 @@ func (a *App) Run(addr string) {
 }
 
 func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/hello", a.hello).Methods("GET")
 	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
 	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
 	a.Router.HandleFunc("/user/{id:[0-9]+}", a.getUser).Methods("GET")
 	a.Router.HandleFunc("/user/{id:[0-9]+}", a.updateUser).Methods("PUT")
 	a.Router.HandleFunc("/user/{id:[0-9]+}", a.deleteUser).Methods("DELETE")
+}
+
+func (a *App) hello(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, http.StatusOK, "hello")
 }
 
 func (a *App) getUsers(w http.ResponseWriter, r *http.Request) {
